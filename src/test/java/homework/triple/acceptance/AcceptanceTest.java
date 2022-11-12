66323e2f5a8faf258c9cf574aa2022da4bb6dbc6
hiro032 <kiryuinh32@gmail.com>
2022-11-12 23:28:32 +0900

    feat: test properties setting

--- src/test/java/homework/triple/acceptance/AcceptanceTest.java
+++ src/test/java/homework/triple/acceptance/AcceptanceTest.java
@@ -5,7 +5,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(locations = "/application-test.properties")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class AcceptanceTest {
 
